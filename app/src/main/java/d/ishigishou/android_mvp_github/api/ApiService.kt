ca68d0e4ded376ca65613e73b15911cf62a9fbe2
石議翔 <shawn@bc3ts.com>
2022-07-05 14:54:01 +0800

    - retorfit 串接 github api 取得資料

---
+++ app/src/main/java/d/ishigishou/android_mvp_github/api/ApiService.kt
@@ -0,0 +1,17 @@
+package d.ishigishou.android_mvp_github.api
+
+import d.ishigishou.android_mvp_github.model.bean.UserListResult
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+
+
+    /**
+     * Github 取得使用者列表
+     */
+    @GET("/users")
+    fun getUsers(@Query("since") since: Int,@Query("per_page") per_page: Int): Observable<ArrayList<UserListResult>>
+}
